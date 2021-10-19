b6315e1ef95372c75a6ee5abb548601b9d85b376
Alan Maldonado <alan.maldonado@gmail.com>
2021-10-19 08:22:23 -0500

    Update naming

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
-const parentheses = require('./index');
+const balancedParens = require('./index');
 
-test('Parentheses is a function', () => {
-  expect(typeof parentheses).toEqual('function');
+test('Balanced Parentheses is a function', () => {
+  expect(typeof balancedParens).toEqual('function');
 });
 
-test('parentheses should return true if balanced', () => {
-  expect(parentheses('()')).toEqual(true);
+test('balancedParens should return true if balanced', () => {
+  expect(balancedParens('()')).toEqual(true);
 });
 
-test('parentheses should return true if not balanced', () => {
-  expect(parentheses(')(')).toEqual(false);
+test('balancedParens should return true if not balanced', () => {
+  expect(balancedParens(')(')).toEqual(false);
 });
